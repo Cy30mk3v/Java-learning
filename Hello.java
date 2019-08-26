4ec322a0928c591f79e9f3d096dfbb86dee94826
Hung Vo Tran Chi <su30mk3v@gmail.com>
2019-08-26 14:29:55 +0700

    first commit

---
+++ Hello.java
@@ -0,0 +1,16 @@
+
+import  java.lang.Math;
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        
+        System.out.println("Hello Java!");
+        System.out.println(123);
+        System.out.print(Math.PI);
+        for(int i=0;i<10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+} 
